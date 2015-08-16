991a298bd16a97302c0ed3dae86612e6e308507d
dvberkel <daan.v.berkel.1980@gmail.com>
2015-08-16 12:11:37 +0200

    Visualize accelerometer data

--- drumstick/js/visualization.js
+++ drumstick/js/visualization.js
@@ -2,12 +2,27 @@
   var Visualizer = $.Visualizer = function(canvas){
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
+    this.context.translate(canvas.width/2, canvas.height/2);
     this.update();
   };
-  Visualizer.prototype.update = function(){
+  Visualizer.prototype.update = function(event){
+    acceleration = (event || {}).acceleration || {
+      'x': 0,
+      'y': 0,
+      'z': 0
+    }
     var ctx = this.context;
+    var width = this.canvas.width;
+    var height = this.canvas.height;
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.fillRect(-width/2, -height/2, width, height);
+    
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(acceleration.x, acceleration.y, 5, 0, 2*Math.PI);
+    ctx.closePath();
+    ctx.fill();
+    
   }
   
   
